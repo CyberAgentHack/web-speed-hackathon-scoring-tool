f843a26645a8c498755ba86657b3d8e65b1ef0c7
nodaguti <nodaguti@gmail.com>
2021-02-05 20:06:39 +0900

    Remove competitors for debugging

--- src/targets.ts
+++ src/targets.ts
@@ -6,30 +6,4 @@ export type Competitor = {
 
 export const targetPaths: string[] = ['/', '/test'];
 
-export const competitors: ReadonlyArray<Competitor> = [
-  {
-    id: 'nodaguti',
-    url: 'https://example.com',
-    eligible: true,
-  },
-  {
-    id: 'kubosho',
-    url: 'https://example.com',
-    eligible: true,
-  },
-  {
-    id: 'xx',
-    url: 'https://example.com',
-    eligible: true,
-  },
-  {
-    id: 'yy',
-    url: 'https://example.com',
-    eligible: false,
-  },
-  {
-    id: 'zz',
-    url: 'https://example.com',
-    eligible: true,
-  },
-];
+export const competitors: ReadonlyArray<Competitor> = [];
